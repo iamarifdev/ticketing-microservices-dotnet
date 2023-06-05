28d5198766233ec8a3593b528eb26daf9955c1bf
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-06-05 12:51:05 +0600

    add auth test cases

--- tests/Ticketing.Auth.Tests/IntegrationTests.cs
+++ tests/Ticketing.Auth.Tests/IntegrationTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,5 +48,41 @@ public class IntegrationTests : IDisposable
         result!.Id.Should().BeGreaterThan(0);
         result.Email.Should().Be(TestDataHelper.TestEmail);
         result.Token.Should().NotBeNullOrEmpty();
+    }
+    
+    [Test]
+    public async Task SignIn_WithInvalidEmailAddress_ReturnsValidationProblem()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var payload = new SignInCommand("invalid_email_address", TestDataHelper.TestPassword);
+
+        // Act
+        var response = await client.PostJsonAsync("/auth/signin", payload);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var result = await response.Content.ReadAsStringAsync();
+        result.Should().NotBeNull();
+        result!.Contains("Email is not valid.").Should().BeTrue();
+    }
+
+    [Test]
+    public void SignIn_WithInvalidValidCredentials_ThrowsUnauthorizedAccessException()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var payload = new SignInCommand(TestDataHelper.TestEmail, "invalid");
+
+        // Act
+        var exception = Assert.ThrowsAsync<UnauthorizedAccessException>(async () =>
+        {
+            var response = await client.PostJsonAsync("/auth/signin", payload);
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        });
+
+        // Assert
+        exception.Should().NotBeNull();
+        exception!.Message.Should().Be("Invalid credentials");
     }
 }
