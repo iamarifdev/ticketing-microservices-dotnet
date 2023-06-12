e2bcc7e3842cdd16b15e14982bc5369d05ae0733
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-06-12 23:05:42 +0600

    common: add global exception middleware

---
+++ src/Ticketing.Common/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Ticketing.Common.Extensions;
+
+namespace Ticketing.Common.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            await context.HandleExceptionAsync(ex);
+        }
+    }
+}
