56deabeef614f0dc4e8c11871f9f3eacd385c0f6
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-06-13 20:24:30 +0600

    common: add dto

---
+++ src/Ticketing.Common/DTO/RabbitMqConfig.cs
@@ -0,0 +1,9 @@
+namespace Ticketing.Common.DTO
+{
+    public class RabbitMqConfig
+    {
+        public string Host { get; set; } = "localhost";
+        public string User { get; set; } = "user";
+        public string Password { get; set; } = "password";
+    }
+}
