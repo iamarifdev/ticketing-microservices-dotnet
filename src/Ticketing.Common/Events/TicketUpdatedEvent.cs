3fbede57728acde22d1664643b0732938ad79441
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-06-14 14:32:55 +0600

    common: update TicketUpdatedEvent.cs

--- src/Ticketing.Common/Events/TicketUpdatedEvent.cs
+++ src/Ticketing.Common/Events/TicketUpdatedEvent.cs
@@ -1,7 +1,7 @@
 namespace Ticketing.Common.Events;
 
 public record TicketUpdatedEvent(
-    string Id, string Version, string Title, decimal Price, string UserId, string? OrderId) : IEvent
+    string Id, string Version, string Title, decimal Price, string UserId) : IEvent
 {
     public Subjects Subject => Subjects.TicketUpdated;
 }
