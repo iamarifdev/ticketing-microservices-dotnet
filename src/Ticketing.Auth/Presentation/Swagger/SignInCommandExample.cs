5ff026639583a124f4ed22263ba5c71f3137c50d
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-06-15 11:15:41 +0600

    auth: add signin swagger payload example

---
+++ src/Ticketing.Auth/Presentation/Swagger/SignInCommandExample.cs
@@ -0,0 +1,10 @@
+using Swashbuckle.AspNetCore.Filters;
+using Ticketing.Auth.Application.Commands;
+
+public class SignInCommandExample : IExamplesProvider<SignInCommand>
+{
+    public SignInCommand GetExamples()
+    {
+        return new SignInCommand("test@test.com", "123456");
+    }
+}
